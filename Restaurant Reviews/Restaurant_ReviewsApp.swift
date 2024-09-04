b0e6ae0b378a81f4d324f5de55b8a3ff92d56e7c
Asher Antrim <aantrim246@cedarville.edu>
2024-09-04 13:43:45 -0400

    Initial Commit

---
+++ Restaurant Reviews/Restaurant_ReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_ReviewsApp.swift
+//  Restaurant Reviews
+//
+//  Created by Asher Antrim on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_ReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
